a3de716c3978edbab2620e97334c8238e5c8da79
danieloh30 <doh@redhat.com>
2021-10-22 09:24:41 -0400

    Live Coding and Unit/Integration Testing

--- enter-serverless-functions/src/main/java/org/acme/GreetingResource.java
+++ enter-serverless-functions/src/main/java/org/acme/GreetingResource.java
@@ -1,20 +1,42 @@
 package org.acme;
 
 import javax.inject.Inject;
-import io.quarkus.funqy.Funq;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+// import io.quarkus.funqy.Funq;
+
+@Path("/hello")
 public class GreetingResource {
 
     @Inject
     GreetingService greetingService;
 
-    @Funq
-    public String greeting(String name) {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(@PathParam String name) {
         return greetingService.greeting(name);
     }
 
-    @Funq
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello Serverless";
     }
+
+    // @Funq
+    // public String greeting(String name) {
+    //     return greetingService.greeting(name);
+    // }
+
+    // @Funq
+    // public String hello() {
+    //     return "Hello Serverless";
+    // }
+
 }
